0fe6d250369f6fd8aae7c438361ca534f10d0232
GertLehmann <GertLehmann@10.50.130.17>
2017-04-28 11:32:20 +0200

    output in main

--- src/databasesp/DatabaseSP.java
+++ src/databasesp/DatabaseSP.java
@@ -17,11 +17,32 @@ public class DatabaseSP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+      
         try {
             DataAccessObjectImpl dao = new DataAccessObjectImpl();
 
-            dao.getTeam(1);
+            System.out.println("--------------------------------------------");
+ 
+            System.out.println("getTeamMembers metode (team #1): ");
+            ArrayList<User> teamMembers = dao.getTeamMembers(1);
+            for (User teamMember : teamMembers) {
+                System.out.println(teamMember.toString());
+            }
+            
+            System.out.println("--------------------------------------------");
+            System.out.println("getTeam metode (team #3):");
+            Team showTeam = dao.getTeam(3);
+            System.out.println("Team = " + showTeam.getName());
+            for (User teamMember : showTeam.getMembers()) {
+                System.out.println(teamMember.toString());
+            }
+            
+            System.out.println("--------------------------------------------");
+            System.out.println("getUser metode (user #7):");
+            User showUser = dao.getUser(7);
+            System.out.println(showUser.toString());
+            System.out.println("--------------------------------------------");
+            
 
         } catch (Exception ex) {
             ex.printStackTrace();
